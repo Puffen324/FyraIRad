6c25c00e564700c8181abcc0661a6ebc44516ab9
André Söderlund <andre.soderlund@live.com>
2021-11-24 14:25:52 +0100

    Test

--- src/com/andresoderlund/main/Main.java
+++ src/com/andresoderlund/main/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		System.out.println("Hejhej");
+		System.out.println("Hejhej");
 		boolean winner = false;
 		int turn = 0;
 		int inputFromUser = 0;
